c68a9c8ffd244a803d34914f68a712ca8a49a614
vivanshu-vvvg <vivanshugupta2003.m@gmail.com>
2023-06-08 22:55:36 +0530

    introducing_firebase

--- src/index.js
+++ src/index.js
@@ -2,8 +2,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// import { initializeApp } from "firebase/app";
+import * as firebase from "firebase";
+import 'firebase/firestore';
 import App from './App';
 import img from './img/shopping-trolley-with-little-snooze_23-2147957113.avif';
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCba9ELn5iKPWD_NgPtrMt2IOPmDIGYqCM",
+  authDomain: "cart-c6967.firebaseapp.com",
+  projectId: "cart-c6967",
+  storageBucket: "cart-c6967.appspot.com",
+  messagingSenderId: "6857718765",
+  appId: "1:6857718765:web:093d6922144c34627405e4"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -15,4 +36,5 @@ root.render(
   </React.StrictMode>
 );
 
-
+// ReactDOM.render(<App/>,document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
